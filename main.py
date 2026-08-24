d5d517f6193114deea4817787507bc7bfe10a91d
AnshuMathuri <anshumathuri964@gmail.com>
2026-08-24 08:13:14 +0530

    Update Index

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ class RAGPipeline:
         self.vector_store = VectorStoreService(
             pinecone_api_key=PINECONE_API_KEY,
             embedder=self.embedder,
-            index_name="multi-query-rag-index",
+            index_name="sentence-transformer-index1",
             
         )
 
